5d48995ea9cf2cdfdc42f2f1c9a4888b02abd7bd
vrjuliao <vrjuliao@hotmail.com>
2022-01-08 20:02:45 -0300

    Finishing root testig

--- tests/test_delta.py
+++ tests/test_delta.py
@@ -1,4 +1,6 @@
-from root_finder.root import calc_delta
+import pytest
+
+from root_finder.root import calc_delta, calc_root
 
 
 def test_delta_zeroed_coeficients():
@@ -27,3 +29,24 @@ def test_zeroed_delta():
 
 def test_positive_delta():
     assert calc_delta(3, 4, 1) > 0
+
+
+def test_non_real_roots():
+    with pytest.raises(Exception) as _:
+        assert calc_root(-1, 1, 0)
+
+def test_root_with_zeroed_a():
+    with pytest.raises(Exception) as _:
+        assert calc_root(-1, 0, 0)
+
+def test_unique_root():
+    assert calc_root(0, 1, -2) == (1, 1)
+
+def test_positive_roots():
+    assert calc_root(1, 1, -2) == (0.5, 1.5)
+
+def test_negative_roots():
+    assert calc_root(1, 1, 2) == (-1.5, -.5)
+
+def test_positive_and_negative_roots():
+    assert calc_root(4, 1, 1) == (-1.5, .5)
