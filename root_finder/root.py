f4cde291431371b59c0d0bf8b1304b07344b95ce
vrjuliao <vrjuliao@hotmail.com>
2022-01-08 19:35:23 -0300

    Code formatting

--- root_finder/root.py
+++ root_finder/root.py
@@ -1,5 +1,6 @@
 def calc_delta(a, b, c):
-    return (b*b) - (4*a*c)
+    return (b * b) - (4 * a * c)
+
 
 def calc_root(delta, a, b):
     if a == 0:
@@ -7,6 +8,5 @@ def calc_root(delta, a, b):
     if delta < 0:
         raise "For a Real result, delta must be greather or equals to 0."
 
-    delta_sqrt = delta**(0.5)
-    return (-b-delta_sqrt)/(2*a), (-b+delta_sqrt)/(2*a)
-    
+    delta_sqrt = delta ** (0.5)
+    return (-b - delta_sqrt) / (2 * a), (-b + delta_sqrt) / (2 * a)
--- tests/test_delta.py
+++ tests/test_delta.py
@@ -2,5 +2,6 @@ import pytest
 
 from root_finder.root import *
 
+
 def test_zeroed_coeficients():
-    assert calc_delta(0,0,0) == 0
+    assert calc_delta(0, 0, 0) == 0
