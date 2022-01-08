e503a10208c8a63119171fefca8c08a72ec87699
vrjuliao <vrjuliao@hotmail.com>
2022-01-08 19:43:27 -0300

    Testing delta

--- tests/test_delta.py
+++ tests/test_delta.py
@@ -1,7 +1,29 @@
-import pytest
-
-from root_finder.root import *
+from root_finder.root import calc_delta
 
 
-def test_zeroed_coeficients():
+def test_delta_zeroed_coeficients():
     assert calc_delta(0, 0, 0) == 0
+
+
+def test_delta_zeroed_b():
+    assert calc_delta(1, 0, 2) == ((-4) * 1 * 2)
+
+
+def test_delta_zeroed_a():
+    assert calc_delta(0, 1, 2) == 1
+
+
+def test_delta_zeroed_c():
+    assert calc_delta(1, 1, 0) == 1
+
+
+def test_negative_delta():
+    assert calc_delta(1, 1, 3) < 0
+
+
+def test_zeroed_delta():
+    assert calc_delta(4, 4, 1) == 0
+
+
+def test_positive_delta():
+    assert calc_delta(3, 4, 1) > 0
